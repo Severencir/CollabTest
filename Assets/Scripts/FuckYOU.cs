1361c56d834d3e6097358d27e4110a0f7bc3460c
DV <david_ventura_m@hotmail.com>
2022-07-23 01:23:54 -0400

    responce to title

--- Assets/Scripts/FuckYOU.cs
+++ Assets/Scripts/FuckYOU.cs
@@ -4,10 +4,11 @@ using UnityEngine;
 
 public class FuckYOU : MonoBehaviour
 {
+    string sev;
     // Start is called before the first frame update
     void Start()
     {
-        
+        sev = "Fuck You";
     }
 
     // Update is called once per frame
